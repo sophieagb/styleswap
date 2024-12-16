9aa6d645d9ce1ad05f151ebd2c0ad9dbc4a6d72c
Sophie <sophiexabla@gmail.com>
2024-12-15 20:09:55 -0500

    made chat page more similar to insta dm's

--- src/ChatPage.js
+++ src/ChatPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link for navigation
+import { Link } from 'react-router-dom';
 import Header from './Main/Header';
 import Footer from './Main/Footer';
 import './ChatPage.css';
@@ -11,18 +11,24 @@ const ChatPage = () => {
       username: '@styleswap_user123',
       avatar: '/images/avatar1.png',
       item: '/images/chat1.png',
+      lastMessage: 'Seen',
+      timestamp: 'Yesterday',
     },
     {
       id: 2,
       username: '@styleswap_user456',
       avatar: '/images/avatar2.png',
       item: '/images/chat2.png',
+      lastMessage: 'Sounds great! Sunday works!',
+      timestamp: '2h ago',
     },
     {
       id: 3,
       username: '@styleswap_user789',
       avatar: '/images/avatar3.png',
-      item: '/images/chat1.png',
+      item: '/images/image1pg1.png',
+      lastMessage: 'Seen',
+      timestamp: '10m ago',
     },
   ];
 
@@ -38,24 +44,23 @@ const ChatPage = () => {
       </div>
       <div className="chat-list">
         {chats.map((chat) => (
-           <Link
-           key={chat.id}
-           to={`/chat/${chat.id}`}
-           state={chat} 
-           className="chat-link"
-         >
+          <Link
+            key={chat.id}
+            to={`/chat/${chat.id}`}
+            state={chat}
+            className="chat-link"
+          >
             <div className="chat-section">
               <div className="avatar">
                 <img src={chat.avatar} alt={`${chat.username} avatar`} />
               </div>
               <div className="chat-info">
                 <span className="username">{chat.username}</span>
-                <div className="item-preview">
-                  <img src={chat.item} alt="Item preview" />
-                </div>
+                <span className="last-message">{chat.lastMessage}</span>
+                <span className="timestamp">{chat.timestamp}</span>
               </div>
-              <div className="arrow">
-                <button className="carousel-button">❯</button>
+              <div className="item-preview">
+                <img src={chat.item} alt="Item preview" />
               </div>
             </div>
           </Link>
